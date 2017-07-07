4af593b595001251a80cf53aec21d0b96b098b3d
Isuru Madusanka <isurum.dev@gmail.com>
2017-07-07 19:17:54 +0530

    Timezone example using Carbon

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    {{ Carbon\Carbon::now() }}
                 </div>
 
                 <div class="links">
