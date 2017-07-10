1ab59f42200b856945c182293993411f9d7e0589
Isuru Madusanka <isurum.dev@gmail.com>
2017-07-10 21:30:24 +0530

    Using Controller show About Us and Contact Pages

---
+++ app/Http/Controllers/PagesController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\User;
+use App\Http\Controllers\Controller;
+
+class PagesController extends Controller
+{
+    /**
+    * Show the welcome view
+    * 
+    * @return Response
+    */
+    public function index()
+    {
+        return view('welcome');
+    }
+
+    /**
+    * Show the about view
+    * 
+    * @return Response
+    */
+    public function showAbout(){
+        return view('about');
+    }
+
+    /**
+    * Show the contact view
+    * 
+    * @return Response
+    */
+    public function showContact(){
+        return view('contact');
+    }
+
+}
