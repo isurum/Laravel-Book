0480f26193db9350b5ec7b1d1b2e858aef21fe4b
Isuru Madusanka <isurum.dev@gmail.com>
2017-07-10 17:57:41 +0530

    Routes Example

--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,13 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('welcome'); 
+});
+
+Route::get('/about', function() {
+    return view('about'); 
+}); 
+
+Route::get('/contact', function() {
+    return view('contact'); 
 });
